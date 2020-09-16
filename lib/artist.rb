1efb67f39ce8cfbbe59521f6dfc4254e1e871963
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-16 21:43:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,18 +8,5 @@ class Artist
  @songs = []
   end
 
-  def self.all
-    @@all
-  end
-
-  def new_song(title, genre)
-    Song.new(title, self, genre)
-  end
-
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end 
 end 
   
