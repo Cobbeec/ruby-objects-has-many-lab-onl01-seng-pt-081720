76d6f75d27759f4d5d26c7a5209b7d9144171620
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-16 23:23:27 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,9 +15,9 @@ def add_post (post)
   post.author = self 
 end   
   
-  def add_post_by_title (post_title)
-    writer = Post.new(post_title)
-    add_post(writer)
+def add_post_by_title (post_name) 
+ writer = Post.new(post_name)
+ add_post(writer)
   end 
   
   
