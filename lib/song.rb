d23fab724a0b99b8f1a67bd5301d510775ec2f06
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-16 21:30:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,21 @@ class Song
   
   @@all = []
   
-  def initalize(name)
-    @name = name 
-    @@all << self 
-  end 
-  
-  def self.all 
+ def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
     @@all
   end
-  
-end 
+end
